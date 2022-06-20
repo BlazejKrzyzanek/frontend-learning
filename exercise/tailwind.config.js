9d251d3098685807cda672bf692ba9f242d8aa56
Błażej Krzyżanek <bl.krzyzanek@gmail.com>
2022-06-20 21:08:56 +0200

    fix some styles

--- exercise/tailwind.config.js
+++ exercise/tailwind.config.js
@@ -11,7 +11,11 @@ module.exports = {
       },
       spacing: {
         '128': '32rem',
-      }
+      },
+      backgroundImage: {
+        'navbar': "url('/assets/header_background.svg')",
+        'section-header': "url(../assets/header_in_text.svg)"
+      },
     },
   },
   plugins: [],
